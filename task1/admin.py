f31115e458a25e7660da99116a0944583c3dc96c
kiper <dima7kiper@mail.ru>
2025-01-20 14:35:05 +0300

    Изменили админку

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Buyer, Auto
+
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    list_filter = ('name', 'age')
+    search_fields = ('name',)
+    list_per_page = 20
+
+
+@admin.register(Auto)
+class AutoAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'year')  # для отображения полей
+    list_filter = ('name', 'color')  # для фильтрации
+    search_fields = ('name',)  # для поиска
+    list_per_page = 30  # количество элементов на странице
+    readonly_fields = ('color',)
